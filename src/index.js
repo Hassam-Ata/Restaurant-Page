ca211df93a9c537eccb223beeb245ab48ea30bed
Hassam-Ata <hassamata86@gmail.com>
2024-04-23 13:56:05 +0500

    created separate file for Home,Menu and About

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+import { loadPage } from './loadPage';
+import { createHomePage } from './createHomePage';
+import { createMenuPage } from './createMenuPage';
+import { createAboutPage } from './createAboutPage';
+loadPage();
+
+const home=document.querySelector(".Home");
+const menu=document.querySelector(".Menu");
+const about=document.querySelector(".About");
+about.addEventListener("click",createAboutPage);
+menu.addEventListener("click",createMenuPage);
+home.addEventListener("click",createHomePage);
---
+++ src/loadPage.js
@@ -0,0 +1,5 @@
+import { createHomePage } from "./createHomePage";
+export function loadPage() {
+    createHomePage();
+    
+}
